5a2ac10412819b9702194554b243c013ea56da9b
Marcel van Eck <vaneck.marcel@gmail.com>
2019-01-12 11:28:50 -0800

    Flake8 edits for scramble.py

--- scramble.py
+++ scramble.py
@@ -2,30 +2,30 @@ import random
 
 
 def scrambleCharacter(c):
-	a1 = 65
-	a2 = 90
-	b1 = 97
-	b2 = 122
-	c1 = 48
-	c2 = 57
+    a1 = 65
+    a2 = 90
+    b1 = 97
+    b2 = 122
+    c1 = 48
+    c2 = 57
 
-	ci = ord(c)
+    ci = ord(c)
 
-	if a1 <= ci <= a2:
-		c = chr(random.randint(a1, a2))
-	elif b1 <= ci <= b2:
-		c = chr(random.randint(b1, b2))
-	elif c1 <= ci <= c2:
-		c = chr(random.randint(c1, c2))
+    if a1 <= ci <= a2:
+        c = chr(random.randint(a1, a2))
+    elif b1 <= ci <= b2:
+        c = chr(random.randint(b1, b2))
+    elif c1 <= ci <= c2:
+        c = chr(random.randint(c1, c2))
 
-	return c
+    return c
 
 
 def scrambleAddress(addr):
-	p1 = addr.find(":")
+    p1 = addr.find(":")
 
-	addrOut = addr[:p1]
-	for c in addr[p1:]:
-		addrOut += scrambleCharacter(c)
+    addrOut = addr[:p1]
+    for c in addr[p1:]:
+        addrOut += scrambleCharacter(c)
 
-	return addrOut
+    return addrOut
